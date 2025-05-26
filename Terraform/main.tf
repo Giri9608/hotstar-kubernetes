b495ee2c73692cb17a3c25d2551a936cc391b433
Giri9608 <129484657+Giri9608@users.noreply.github.com>
2025-05-26 17:00:35 +0530

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -23,10 +23,10 @@ resource "aws_default_vpc" "default" {
 
 # Create a subnet in the default VPC
 resource "aws_subnet" "main" {
-  vpc_id            = aws_default_vpc.default.id
-  cidr_block        = "172.31.100.0/24"  # Adjust CIDR block as needed, ensure it fits within the VPC's CIDR
-  availability_zone = "ap-south-1a"      # Specify an availability zone in ap-south-1
-  map_public_ip_on_launch = true         # Enable public IP for instances in this subnet
+  vpc_id                  = aws_default_vpc.default.id
+  cidr_block              = "172.31.100.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
 
   tags = {
     Name = "monitoring-subnet"
@@ -37,9 +37,8 @@ resource "aws_subnet" "main" {
 resource "aws_security_group" "ec2_security_group" {
   name        = "ec2 security group"
   description = "allow access on ports 22"
-  vpc_id      = aws_default_vpc.default.id  # Associate with the default VPC
+  vpc_id      = aws_default_vpc.default.id
 
-  # Allow access on port 22
   ingress {
     description = "ssh access"
     from_port   = 22
@@ -62,11 +61,11 @@ resource "aws_security_group" "ec2_security_group" {
 
 # Create EC2 instance
 resource "aws_instance" "Monitoring_server" {
-  ami             = "ami-00bb6a80f01f03502"
-  instance_type   = "t2.medium"
-  subnet_id       = aws_subnet.main.id  # Reference the created subnet
-  security_groups = [aws_security_group.ec2_security_group.name]
-  key_name        = var.key_name
+  ami                    = "ami-00bb6a80f01f03502"
+  instance_type          = "t2.medium"
+  subnet_id              = aws_subnet.main.id
+  vpc_security_group_ids = [aws_security_group.ec2_security_group.id]  # Use vpc_security_group_ids
+  key_name               = var.key_name
 
   tags = {
     Name = var.instance_name
